7a706b7eddee8baef7772cca213d72ef34741d35
Dani <dani10.rowe@gmail.com>
2022-03-28 00:43:52 -0400

    mostly done lab

--- src/components/labs/todo/TodoItem.js
+++ src/components/labs/todo/TodoItem.js
@@ -1,3 +1,4 @@
+import React from 'react'
 const TodoItem = ({
   //default object for testing
   todo = {
--- src/components/labs/todo/index.js
+++ src/components/labs/todo/index.js
@@ -5,7 +5,7 @@
 //        <h1>Todo example</h1>
 //    </div>
 //`);
-
+import React from 'react'
 import TodoList from "./TodoList.js";
 
 $('#wd-todo').append(`
