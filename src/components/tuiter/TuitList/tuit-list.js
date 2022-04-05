b87653a537a91e11ac5c1ce5de7cd53d6aa80b4c
Dani <dani10.rowe@gmail.com>
2022-04-04 22:30:14 -0400

    data from server - tuits

--- src/components/tuiter/TuitList/tuit-list.js
+++ src/components/tuiter/TuitList/tuit-list.js
@@ -1,22 +1,53 @@
-import React from "react";
-import {useSelector} from "react-redux";
-import TuitListItem
-       from "./tuit-list-item";
-// import './tuits.css';
+import React, {useEffect} from "react";
+import {useDispatch, useSelector}
+  from "react-redux";
+import TuitListItem from "./tuit-list-item"
+import * as service
+  from './../services/tuits-service';
 
 const TuitList = () => {
-const tuits = useSelector(
-    state => state.tuits);
+ const tuits = useSelector(
+       state => state.tuits);
 
-  return (
-    <ul className="ttr-tuits list-group">
-      {
-        tuits.map && tuits.map(tuit =>
-          <TuitListItem key={tuit._id}
-                        tuit={tuit}/>)
-      }
-    </ul>
-  );
+ const dispatch = useDispatch();
+
+ const findAllTuits = async () => {
+   const tuits = await service.findAllTuits();
+   dispatch({
+     type: 'FIND_ALL_TUITS',
+     tuits: tuits
+   });
+ }
+ useEffect(findAllTuits, []);
+ return ( <ul className="ttr-tuits list-group">
+                {
+                  tuits.map && tuits.map(tuit =>
+                    <TuitListItem key={tuit._id}
+                                  tuit={tuit}/>)
+                }
+              </ul> );
 }
-
 export default TuitList;
+
+//import React from "react";
+//import {useSelector} from "react-redux";
+//import TuitListItem
+//       from "./tuit-list-item";
+//// import './tuits.css';
+//
+//const TuitList = () => {
+//const tuits = useSelector(
+//    state => state.tuits);
+//
+//  return (
+//    <ul className="ttr-tuits list-group">
+//      {
+//        tuits.map && tuits.map(tuit =>
+//          <TuitListItem key={tuit._id}
+//                        tuit={tuit}/>)
+//      }
+//    </ul>
+//  );
+//}
+//
+//export default TuitList;
