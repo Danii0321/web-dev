08cd862817fe693d9c55abae3d2439884bdf7dfc
Dani <dani10.rowe@gmail.com>
2022-04-06 14:02:45 -0400

    6.2

--- src/components/tuiter/TuitList/tuit-list.js
+++ src/components/tuiter/TuitList/tuit-list.js
@@ -2,8 +2,8 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector}
   from "react-redux";
 import TuitListItem from "./tuit-list-item"
-import * as service
-  from './../services/tuits-service';
+import {findAllTuits}
+  from "./../actions/tuits-actions";
 
 const TuitList = () => {
  const tuits = useSelector(
@@ -11,14 +11,10 @@ const TuitList = () => {
 
  const dispatch = useDispatch();
 
- const findAllTuits = async () => {
-   const tuits = await service.findAllTuits();
-   dispatch({
-     type: 'FIND_ALL_TUITS',
-     tuits: tuits
-   });
- }
- useEffect(findAllTuits, []);
+ useEffect(() =>
+   findAllTuits(dispatch),
+   []);
+
  return ( <ul className="ttr-tuits list-group">
                 {
                   tuits.map && tuits.map(tuit =>
@@ -28,26 +24,3 @@ const TuitList = () => {
               </ul> );
 }
 export default TuitList;
-
-//import React from "react";
-//import {useSelector} from "react-redux";
-//import TuitListItem
-//       from "./tuit-list-item";
-//// import './tuits.css';
-//
-//const TuitList = () => {
-//const tuits = useSelector(
-//    state => state.tuits);
-//
-//  return (
-//    <ul className="ttr-tuits list-group">
-//      {
-//        tuits.map && tuits.map(tuit =>
-//          <TuitListItem key={tuit._id}
-//                        tuit={tuit}/>)
-//      }
-//    </ul>
-//  );
-//}
-//
-//export default TuitList;
--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,60 +1,13 @@
 // import tuits from "../data/tuits.json";
+import {FIND_ALL_TUITS}
+  from "../actions/tuits-actions";
 
 const tuitsReducer = (state = [], action) => {
- switch (action.type) {
-   case 'FIND_ALL_TUITS':
+switch (action.type) {
+   case FIND_ALL_TUITS:
      return action.tuits;
    default:
      return state;
  }
 }
 export default tuitsReducer;
-
-//  case 'like-tuit':
-//        return state.map(tuit => {
-//          if(tuit._id === action.tuit._id) {
-//            if(tuit.liked === true) {
-//              tuit.liked = false;
-//              tuit.stats.likes--;
-//            } else {
-//              tuit.liked = true;
-//              tuit.stats.likes++;
-//            }
-//            return tuit;
-//          } else {
-//            return tuit;
-//          }
-//        });
-//
-//    case 'delete-tuit':
-//        return state.filter(
-//          tuit => tuit._id !== action.tuit._id);
-//    case 'create-tuit':
-//      const newTuit = {
-//        tuit: action.tuit,
-//        _id: (new Date()).getTime() + '',
-//        postedBy: {
-//          "username": "Dani"
-//        },
-//        "avatar-image": "../../../../tuiter/images/dani.jpg",
-//        "liked": false,
-//        "verified": false,
-//        "handle": "Dani",
-//        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-//        "time": "now",
-//        stats: {
-//          retuits: 111,
-//          likes: 222,
-//          replies: 333
-//        }
-//      }
-//      return [
-//        newTuit,
-//        ...state,
-//      ];
-//    default:
-//      return tuits
-//  }
-//}
-//
-//export default tuitsReducer;
